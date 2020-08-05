b18d41abd01ebfd5c02f935b8b4bbc8027618ff4
KirillØleynik <kirill.olejnik@gmail.com>
2020-08-05 15:55:53 +0300

    Add RSpec JUnit Formatter to fix CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  config.add_formatter 'Fuubar'
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
