255fc1e74e6a64fc3c7dbe6c058869bb7cad8705
KirillØleynik <kirill.olejnik@gmail.com>
2020-08-15 11:36:51 +0300

    Update routes. Add endpoints to crud authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  with_options only: [:create,:update,:destroy] do |admin_only|
+    admin_only.resources :authors
+  end
+    namespace :api, path: '/', constraints: {subdomain: 'api'} do
+  with_options only: [:index,:show] do |list_only|
+    list_only.resources :authors
+  end
+    end
 end
